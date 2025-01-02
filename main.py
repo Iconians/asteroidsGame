cf3a6949a8060fc7574a41aa7e3fd3ea10e2ed12
Iconians <claytoncripe@gmail.com>
2025-01-02 15:38:05 -0800

    hooked up the left/right of the update function

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+from player import Player
 
 
 def main():
     print("Starting asteroids!")
     print("Screen width: " + str(SCREEN_WIDTH))
     print("Screen height: " + str(SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-              return
-            
-        
-    
+              return 
+        screen.fill((0, 0, 0))
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
-    
+        dt = clock.tick(60) /1000
+
 
 
 if __name__ == "__main__":
