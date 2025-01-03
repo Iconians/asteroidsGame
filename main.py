d8475d73357f3aebc133deae7e6a654e00df4892
Iconians <claytoncripe@gmail.com>
2025-01-03 13:34:10 -0800

    got the asteroids showing and floating around

--- main.py
+++ main.py
@@ -2,6 +2,18 @@ import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
+# groups
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+asteroids = pygame.sprite.Group()
+
+Player.containers = (updatable, drawable)
+Asteroid.containers = (asteroids, updatable, drawable)
+AsteroidField.containers = (updatable,)
+
 
 
 def main():
@@ -11,14 +23,19 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
               return 
         screen.fill((0, 0, 0))
-        player.update(dt)
-        player.draw(screen)
+
+        for obj in updatable:
+            obj.update(dt)
+        
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) /1000
 
--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -17,6 +17,11 @@ class Player(CircleShape):
     
     def rotate(self, dt):
         self.rotation +=  PLAYER_TURN_SPEED * dt
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        movement = forward * PLAYER_SPEED * dt
+        self.position += movement
     
     def update(self, dt):
         keys = pygame.key.get_pressed()
@@ -25,7 +30,10 @@ class Player(CircleShape):
               self.rotate(-dt)
         if keys[pygame.K_d]:
               self.rotate(dt)
-
+        if (keys[pygame.K_w]):
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
     
     def draw(self, screen):
       pygame.draw.polygon(screen, "white", self.triangle(), 2)
